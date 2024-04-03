814bd7a8ca311939a6a6fe9643cf93b54a45c55a
giovanni.frison <giovanni.frison@alten.it>
2024-04-03 11:36:38 +0200

    Functional GET with validation and exception handler

--- webflux-101/src/main/java/org/fdifrison/webflux101/config/RequestHandler.java
+++ webflux-101/src/main/java/org/fdifrison/webflux101/config/RequestHandler.java
@@ -1,7 +1,9 @@
 package org.fdifrison.webflux101.config;
 
+import org.fdifrison.webflux101.dto.InputFailedValidationResponse;
 import org.fdifrison.webflux101.dto.MultiplyRequest;
 import org.fdifrison.webflux101.dto.Response;
+import org.fdifrison.webflux101.exception.InputValidationException;
 import org.fdifrison.webflux101.service.ReactiveMathService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
@@ -23,15 +25,13 @@ public class RequestHandler {
     public Mono<ServerResponse> squareHandler(ServerRequest serverRequest) {
         int input = Integer.parseInt(serverRequest.pathVariable("input"));
         Mono<Response> square = service.findSquare(input);
-        return ServerResponse.ok()
-                .body(square, Response.class);
+        return ServerResponse.ok().body(square, Response.class);
     }
 
     public Mono<ServerResponse> tableHandler(ServerRequest serverRequest) {
         int input = Integer.parseInt(serverRequest.pathVariable("input"));
         Flux<Response> multiplicationTable = service.findMultiplicationTable(input);
-        return ServerResponse.ok()
-                .body(multiplicationTable, Response.class);
+        return ServerResponse.ok().body(multiplicationTable, Response.class);
     }
 
     public Mono<ServerResponse> tableStreamHandler(ServerRequest serverRequest) {
@@ -45,7 +45,15 @@ public class RequestHandler {
     public Mono<ServerResponse> multiplyHandler(ServerRequest serverRequest) {
         Mono<MultiplyRequest> multiplyRequestMono = serverRequest.bodyToMono(MultiplyRequest.class);
         Mono<Response> multiply = service.multiply(multiplyRequestMono);
-        return ServerResponse.ok()
-                .body(multiply, Response.class);
+        return ServerResponse.ok().body(multiply, Response.class);
+    }
+
+    public Mono<ServerResponse> squareHandlerWithValidation(ServerRequest serverRequest) {
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+        if (input < 10 || input > 20) {
+            return Mono.error(new InputValidationException(input));
+        }
+        Mono<Response> square = service.findSquare(input);
+        return ServerResponse.ok().body(square, Response.class);
     }
 }
--- webflux-101/src/main/java/org/fdifrison/webflux101/config/RouterConfig.java
+++ webflux-101/src/main/java/org/fdifrison/webflux101/config/RouterConfig.java
@@ -1,10 +1,16 @@
 package org.fdifrison.webflux101.config;
 
+import org.fdifrison.webflux101.dto.InputFailedValidationResponse;
+import org.fdifrison.webflux101.exception.InputValidationException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.function.BiFunction;
 
 @Configuration
 public class RouterConfig {
@@ -19,10 +25,21 @@ public class RouterConfig {
     public RouterFunction<ServerResponse> serverResponseRouterFunction() {
         return RouterFunctions.route()
                 .GET("router/square/{input}", handler::squareHandler)
-                .GET("router/table/{input}", handler::tableHandler)
+                .GET("router/squareValid/{input}", handler::squareHandlerWithValidation)
+                .GET("router/table/{input" + "}", handler::tableHandler)
                 .GET("router/tableStream/{input}", handler::tableStreamHandler)
                 .POST("router/multiply", handler::multiplyHandler)
-                .build();
+                .onError(InputValidationException.class, exceptionHandler()).build();
+    }
+
+    // works as a controllerAdvice
+    private BiFunction<Throwable, ServerRequest, Mono<ServerResponse>> exceptionHandler() {
+        return (err, req) -> {
+            InputValidationException ex = (InputValidationException) err;
+            InputFailedValidationResponse response = new InputFailedValidationResponse(ex.getInput(),
+                    ex.getErrorCode(), ex.getMessage());
+            return ServerResponse.badRequest().bodyValue(response);
+        };
     }
 
 }
