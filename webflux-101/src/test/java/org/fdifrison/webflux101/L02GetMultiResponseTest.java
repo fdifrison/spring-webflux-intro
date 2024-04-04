ddb312b7d912dfa288175230c7af17bd88d29141
giovanni.frison <giovanni.frison@alten.it>
2024-04-04 09:44:22 +0200

    testing a flux stream response

--- webflux-101/src/test/java/org/fdifrison/webflux101/L02GetMultiResponseTest.java
+++ webflux-101/src/test/java/org/fdifrison/webflux101/L02GetMultiResponseTest.java
@@ -25,4 +25,21 @@ public class L02GetMultiResponseTest extends BaseTest {
 
     }
 
+    @Test
+    public void fluxStreamStepVerifierTest() {
+
+        Flux<Response> response = this.webClient
+                .get()
+                .uri("r-math/table/{input}/stream", 5)
+                .retrieve()
+                .bodyToFlux(Response.class)
+                .doOnNext(System.out::println);
+
+        StepVerifier.create(response)
+                .expectNextCount(10)
+                .verifyComplete();
+
+
+    }
+
 }
