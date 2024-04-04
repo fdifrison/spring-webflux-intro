caf79bb76a938cfbbd8705965e86916994340857
giovanni.frison <giovanni.frison@alten.it>
2024-04-04 09:33:50 +0200

    implementing test using stepVerifier

--- webflux-101/src/test/java/org/fdifrison/webflux101/L01GetSingleResponseTest.java
+++ webflux-101/src/test/java/org/fdifrison/webflux101/L01GetSingleResponseTest.java
@@ -4,6 +4,8 @@ import org.fdifrison.webflux101.dto.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 public class L01GetSingleResponseTest extends BaseTest {
 
@@ -24,6 +26,23 @@ public class L01GetSingleResponseTest extends BaseTest {
 
     }
 
+
+    @Test
+    public void stepVerifierTest() {
+
+        Mono<Response> response = this.webClient
+                .get()
+                .uri("r-math/square/{input}", 5)
+                .retrieve()
+                .bodyToMono(Response.class);
+
+        StepVerifier.create(response)
+                .expectNextMatches(r -> r.output() == 25)
+                .verifyComplete();
+
+        System.out.println(response.block());
+
+    }
 
 
 }
