6b5bc570fba6657944538144f21da208f8a4bc5c
giovanni.frison <giovanni.frison@alten.it>
2024-04-04 11:49:28 +0200

    passing custom Bearer JWT Token

--- webflux-101/src/test/java/org/fdifrison/webflux101/config/WebClientConfig.java
+++ webflux-101/src/test/java/org/fdifrison/webflux101/config/WebClientConfig.java
@@ -2,6 +2,8 @@ package org.fdifrison.webflux101.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -11,7 +13,23 @@ public class WebClientConfig {
     public WebClient webClient() {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080")
-                .defaultHeaders(h -> h.setBasicAuth("user", "pwd"))
+                //                .defaultHeaders(h -> h.setBasicAuth("user", "pwd"))
+                .filter(sessionTokenGenerator())
                 .build();
     }
+
+
+    private ExchangeFilterFunction sessionTokenGenerator() {
+        System.out.println("Generating Session Token:");
+        return (request, next) -> {
+            ClientRequest token = ClientRequest.from(request).headers(h -> h.setBearerAuth("jwt-token")).build();
+            System.out.println(token.headers());
+            return next.exchange(token);
+        };
+
+
+    }
+
+
 }
+
