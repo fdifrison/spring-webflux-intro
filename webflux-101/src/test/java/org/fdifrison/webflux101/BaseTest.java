cae7027ca6fe5eae30db5870685b58c8b41760ac
giovanni.frison <giovanni.frison@alten.it>
2024-04-04 09:39:48 +0200

    testing a flux response

--- webflux-101/src/test/java/org/fdifrison/webflux101/BaseTest.java
+++ webflux-101/src/test/java/org/fdifrison/webflux101/BaseTest.java
@@ -1,9 +1,15 @@
 package org.fdifrison.webflux101;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
 
+// Start the application on the same port each time we run test
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class BaseTest {
+
+    @Autowired
+    protected WebClient webClient;
 
 
 }
--- webflux-101/src/test/java/org/fdifrison/webflux101/L01GetSingleResponseTest.java
+++ webflux-101/src/test/java/org/fdifrison/webflux101/L01GetSingleResponseTest.java
@@ -2,15 +2,10 @@ package org.fdifrison.webflux101;
 
 import org.fdifrison.webflux101.dto.Response;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 public class L01GetSingleResponseTest extends BaseTest {
-
-    @Autowired
-    private WebClient webClient;
 
     @Test
     public void blockTest() {
