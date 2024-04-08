952a09c5db3046fb5d4ca1c4a580138dc03cc669
giovanni.frison <giovanni.frison@alten.it>
2024-04-08 17:48:18 +0200

    removed mock project 101

--- webflux-101/src/main/java/org/fdifrison/webflux101/dto/InputFailedValidationResponse.java
+++
@@ -1,5 +0,0 @@
-package org.fdifrison.webflux101.dto;
-
-public record InputFailedValidationResponse(int errorCode, int input, String msg) {
-
-}
--- webflux-101/src/main/java/org/fdifrison/webflux101/dto/MultiplyRequest.java
+++
@@ -1,4 +0,0 @@
-package org.fdifrison.webflux101.dto;
-
-public record MultiplyRequest(int first, int second) {
-}
