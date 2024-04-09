50441db2e98d3c69760f214a40fd5aaafbae635a
giovanni.frison <giovanni.frison@alten.it>
2024-04-09 09:36:18 +0200

    handling service unavailability

--- order-service/src/main/java/com/example/orderservice/controller/PurchaseOrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/PurchaseOrderController.java
@@ -4,8 +4,11 @@ import com.example.orderservice.dto.OrderRequestDto;
 import com.example.orderservice.dto.OrderResponseDto;
 import com.example.orderservice.service.OrderFulfillmentService;
 import com.example.orderservice.service.OrderQueryService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -26,6 +29,9 @@ public class PurchaseOrderController {
     public Mono<ResponseEntity<OrderResponseDto>> create(@RequestBody Mono<OrderRequestDto> orderRequestDtoMono) {
         return this.service.purchaseOrder(orderRequestDtoMono)
                 .map(response -> ResponseEntity.ok().body(response))
+                .onErrorReturn(WebClientResponseException.class, ResponseEntity.badRequest().build())
+                .onErrorReturn(WebClientRequestException.class,
+                        ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build())
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
