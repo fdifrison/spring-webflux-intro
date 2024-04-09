c35020dff0a4cb3f95b3972867a119a2dd36ce60
giovanni.frison <giovanni.frison@alten.it>
2024-04-09 11:10:37 +0200

    simple front-end with event-listener on the streaming api with filter

--- product-service/src/main/java/org/fdifrison/productservice/controller/ProductStreamController.java
+++ product-service/src/main/java/org/fdifrison/productservice/controller/ProductStreamController.java
@@ -3,9 +3,12 @@ package org.fdifrison.productservice.controller;
 import org.fdifrison.productservice.dto.ProductDto;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("product")
@@ -17,10 +20,11 @@ public class ProductStreamController {
         this.productStream = productStream;
     }
 
-    @GetMapping(value = "stream",
+    @GetMapping(value = "stream/{maxPrice}",
             produces = MediaType.TEXT_EVENT_STREAM_VALUE) // needed to become a streaming api
-    public Flux<ProductDto> getProductsUpdate() {
-        return productStream;
+    public Flux<ProductDto> getProductsUpdate(@PathVariable BigDecimal maxPrice) {
+        return productStream
+                .filter(dto -> dto.price().compareTo(maxPrice) < 0);
     }
 
 
